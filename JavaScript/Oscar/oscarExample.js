6a449601211472228c2e01a5332a96a8659baec5
Hugo Cabral Tannús <hctannus@gmail.com>
2024-09-03 12:04:05 -0300

    Documenta sequência de estados para resolução do exercício.

--- JavaScript/Oscar/oscarExample.js
+++ JavaScript/Oscar/oscarExample.js
@@ -8,4 +8,59 @@ const { csv2JSON } = require('csv');
 const objJson = csv2JSON(csvOscar);
 
 console.log(objJson);
-console.table(objJson);
+// console.table(objJson);
+console.table(objJson, ["Filme", "Diretor", "Ano"]);
+
+
+
+/* Estado A: CSV
+---------
+`nome;idade
+João;28
+Maria;32`
+*/
+
+/* Estado D: Lista de Strings (linhas)
+---------
+[
+    "nome;idade",
+    "João;28",
+    "Maria;32"
+]
+*/
+
+/* Estado E: Lista de colunas
+---------
+[
+    ["nome", "idade"],
+    ["João", "28"],
+    ["Maria","32"]
+]
+*/
+
+/* Estado F: Cabeçalho
+---------
+["nome", "idade"]
+*/
+
+/* Estado G: Conteúdo da tabela (lista de colunas)
+---------
+[
+    ["João", "28"],
+    ["Maria","32"]
+]
+*/
+
+/* Estado C: Objeto JSON
+---------
+[
+    {
+        nome: "João",
+        idade: "28"
+    },
+    {
+        nome: "Maria",
+        idade: "32"
+    },
+]
+*/
