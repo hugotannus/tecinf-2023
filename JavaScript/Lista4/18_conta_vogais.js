c3e77cf6f5aa65f5f6b2216b82f43d081d8310a4
Hugo Cabral Tannús <hctannus@gmail.com>
2024-09-11 11:52:43 -0300

    Adiciona `node_modules` ao arquivo `.gitignore`

--- JavaScript/Lista4/18_conta_vogais.js
+++ JavaScript/Lista4/18_conta_vogais.js
@@ -1,4 +1,11 @@
-const { contaVogais } = require("lista4")
+const vogais = ["a", "e", "i", "o", "u"];
+
+exports.contaVogais = function contaVogais(texto) {
+    let letras = texto.split('');
+    let result = letras.filter(letra => vogais.includes(letra));
+
+    return result.length;
+}
 
 console.log(contaVogais("hello"));
 console.log(contaVogais("aeiou"));
