02ae2d3c85aa04ad1d418c38017ded8ec6fb650c
Hugo Cabral Tannús <hctannus@gmail.com>
2024-09-13 11:50:29 -0300

    Busca 3 cartas no topo do deck e exibe na tela

--- JavaScript/Truco/truco.js
+++ JavaScript/Truco/truco.js
@@ -1,12 +1,28 @@
-function converteParaJson(data) {
-    const obj = data.json();
-    return obj;
-}
-function imprimeTabela(json) {
-    console.table(json.cards, ["code", "image", "value", "suit"]);
-    console.log("restam", json.remaining, "cartas")
-}
+function olharCartas() {
+    fetch('https://deckofcardsapi.com/api/deck/4sff83670lab/draw/?count=3')
+    .then(converteParaJson)
+    .then(imprimeTabela)
+    .then(mostraCartas);
 
-fetch('https://deckofcardsapi.com/api/deck/4sff83670lab/draw/?count=3')
-.then(converteParaJson)
-.then(imprimeTabela);
+    function converteParaJson(data) {
+        const obj = data.json();
+        return obj;
+    }
+
+    function imprimeTabela(json) {
+        console.table(json.cards, ["code", "image", "value", "suit"]);
+        console.log("restam", json.remaining, "cartas")
+        
+        return json.cards;
+    }
+    
+    function mostraCartas(cards) {
+        const img0 = document.getElementById('carta0');
+        const img1 = document.getElementById('carta1');
+        const img2 = document.getElementById('carta2');
+        
+        img0.setAttribute('src', cards[0].image);
+        img1.setAttribute('src', cards[1].image);
+        img2.setAttribute('src', cards[2].image);
+    }
+}
