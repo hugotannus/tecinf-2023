6dc7d539c5af2e775ad6562c11b35d5fe854d3a3
Hugo Cabral Tannús <hctannus@gmail.com>
2024-09-17 12:38:36 -0300

    Refatoração de código.

--- JavaScript/Truco/truco.js
+++ JavaScript/Truco/truco.js
@@ -1,28 +1,65 @@
 function olharCartas() {
-    fetch('https://deckofcardsapi.com/api/deck/4sff83670lab/draw/?count=3')
-    .then(converteParaJson)
-    .then(imprimeTabela)
-    .then(mostraCartas);
+    fetch("https://deckofcardsapi.com/api/deck/4sff83670lab/draw/?count=3")
+        .then(getJsonData)
+        .then(atualizaInfo)
+        .then(imprimeTabela)
+        .then(mostraCartas)
+        .catch(erro => alert(erro));
+}
 
-    function converteParaJson(data) {
-        const obj = data.json();
-        return obj;
-    }
+function reembaralhar() {
+    fetch("https://deckofcardsapi.com/api/deck/4sff83670lab/shuffle/?remaining=false")
+        .then(getJsonData)
+        .then(atualizaInfo)
+        .catch(erro => alert(erro));
+}
 
-    function imprimeTabela(json) {
-        console.table(json.cards, ["code", "image", "value", "suit"]);
-        console.log("restam", json.remaining, "cartas")
-        
-        return json.cards;
-    }
+function getJsonData(response) {
+    return response.json();
+}
+
+function atualizaInfo(jsonData) {
+    if (!jsonData.success)
+        throw new Error("Oops! Tente reembaralhar as cartas");
+
+    const deck_id = jsonData.deck_id;
+    const remaining = jsonData.remaining;
+
+    const info1 = document.getElementById("deck_id");
+    const info2 = document.getElementById("remaining");
+
+    info1.innerText = deck_id;
+    info2.innerText = remaining;
+
+    return jsonData;
+}
+
+function imprimeTabela(json) {
+    console.table(json.cards, ["code", "image", "value", "suit"]);
+    console.log("restam", json.remaining, "cartas")
+
+    return json;
+}
+
+function constroiCarta(cardId, cardImage) {
+    const card = document.getElementById(cardId);
     
-    function mostraCartas(cards) {
-        const img0 = document.getElementById('carta0');
-        const img1 = document.getElementById('carta1');
-        const img2 = document.getElementById('carta2');
-        
-        img0.setAttribute('src', cards[0].image);
-        img1.setAttribute('src', cards[1].image);
-        img2.setAttribute('src', cards[2].image);
+    let img = card.querySelector('img');
+    if (img == null) img = document.createElement('img');
+    
+    img.setAttribute('src', cardImage);
+    card.append(img);
+}
+
+function mostraCartas(jsonData) {
+    const cards = jsonData.cards;
+    
+    for (let i=0; i < cards.length; i++) {
+        const cardId = `carta${i + 1}`;
+        const cardImage = cards[i].image;
+
+        constroiCarta(cardId, cardImage);
     }
+
+    return true;
 }
