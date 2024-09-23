228eae07d740747e3d350dedd7c4c86ae9415cfe
Prof. Hugo <hugo.tannus@go.senac.br>
2024-09-23 11:04:42 -0300

    Incorpora demais funções como métodos privados para a classe `Deck`.

--- JavaScript/Truco/truco.js
+++ JavaScript/Truco/truco.js
@@ -1,91 +1,83 @@
 const deckId = "4sff83670lab";
 const deck = new Deck(deckId);
-// deck.setDeckId({ deckId: 123 });
 
 function Deck(deckId) {
-    this.deckId = deckId;
-    this.listOfCards = "4c,7h,7d,3c,3h,3s,3d,2c,2h,2s,2d,jc,jh,js,jd,kc,kh,ks,kd,qc,qh,qs,Qd,Ac,Ah,As,Ad";
+    let deckId = deckId;
+    let listOfCards = "4c,7h,7d,3c,3h,3s,3d,2c,2h,2s,2d,jc,jh,js,jd,kc,kh,ks,kd,qc,qh,qs,Qd,Ac,Ah,As,Ad";
 
-    /* Funções de acesso à API */
+    /* Métodos públicos */
 
     this.gerarBaralho = function () {
-        fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?cards=${this.listOfCards}`)
-        .then(getJsonData)
-        .then(atualizaInfo)
-        .then(setDeckId)
-        .catch(erro => alert(erro));
+        fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?cards=${listOfCards}`)
+            .then(function (response) {
+                const jsonData = response.json();
+                return jsonData;
+            })
+            .then(atualizaInfo)
+            .then(setDeckId)
+            .catch(erro => alert(erro));
     }
 
-    this.olharCartas = function() {
-        fetch(`https://deckofcardsapi.com/api/deck/${this.deckId}/draw/?count=3`)
-            .then(getJsonData)
+    this.olharCartas = function () {
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`)
+            .then((response) => {
+                return response.json()
+            })
             .then(atualizaInfo)
-            .then(imprimeTabela)
             .then(mostraCartas)
             .catch(erro => alert(erro));
     }
 
-    this.reembaralhar = function() {
-        fetch(`https://deckofcardsapi.com/api/deck/${this.deckId}/shuffle/?remaining=false`)
-            .then(getJsonData)
+    this.reembaralhar = function () {
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/?remaining=false`)
+            .then(response => response.json())
             .then(atualizaInfo)
             .catch(erro => alert(erro));
     }
+
+    /* Métodos Privados */
 
     const setDeckId = (jsonData) => {
         this.deckId = jsonData.deck_id;
         return jsonData;
     }
-}
 
-/* Funções Privadas */
+    const atualizaInfo = (jsonData) => {
+        if (!jsonData.success)
+            throw new Error("Oops! Tente reembaralhar as cartas");
 
-function getJsonData(response) {
-    return response.json();
-}
+        const deck_id = jsonData.deck_id;
+        const remaining = jsonData.remaining;
 
-function atualizaInfo(jsonData) {
-    if (!jsonData.success)
-        throw new Error("Oops! Tente reembaralhar as cartas");
+        const info1 = document.getElementById("deck_id");
+        const info2 = document.getElementById("remaining");
 
-    const deck_id = jsonData.deck_id;
-    const remaining = jsonData.remaining;
+        info1.innerText = deck_id;
+        info2.innerText = remaining;
 
-    const info1 = document.getElementById("deck_id");
-    const info2 = document.getElementById("remaining");
-
-    info1.innerText = deck_id;
-    info2.innerText = remaining;
-
-    return jsonData;
-}
-
-function imprimeTabela(json) {
-    console.table(json.cards, ["code", "image", "value", "suit"]);
-    console.log("restam", json.remaining, "cartas")
-
-    return json;
-}
-
-function constroiCarta(cardId, cardImage) {
-    const card = document.getElementById(cardId);
-
-    let img = card.querySelector('img');
-    if (img == null) img = document.createElement('img');
-
-    img.setAttribute('src', cardImage);
-    card.append(img);
-}
-
-function mostraCartas(jsonData) {
-    const cards = jsonData.cards;
-
-    for (let i=0; i < cards.length; i++) {
-        const cardId = `carta${i + 1}`;
-        const cardImage = cards[i].image;
-
-        constroiCarta(cardId, cardImage);
+        return jsonData;
     }
 
-    return true;
+    function constroiCarta(cardId, cardImage) {
+        const card = document.getElementById(cardId);
+
+        let img = card.querySelector('img');
+        if (img == null) img = document.createElement('img');
+
+        img.setAttribute('src', cardImage);
+        card.append(img);
+    }
+
+    function mostraCartas(jsonData) {
+        const cards = jsonData.cards;
+
+        for (let i = 0; i < cards.length; i++) {
+            const cardId = `carta${i + 1}`;
+            const cardImage = cards[i].image;
+
+            constroiCarta(cardId, cardImage);
+        }
+
+        return true;
+    }
 }
